d18bd4bbdf2e03f4463618b5e48692a192697f92
Huiying0108 <whymissu1228@gmail.com>
2019-03-11 14:08:06 -0700

    first commit

---
+++ client/build/precache-manifest.186e39025b7d551d7e5f900f06d53e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03fab382263524b3b07b4467dabd332a",
+    "url": "/static/media/books-2.03fab382.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7738be2ed5e9eda5ba72",
+    "url": "/static/js/main.2fd79083.chunk.js"
+  },
+  {
+    "revision": "5944f05e92909bc59792",
+    "url": "/static/js/2.ff7c030a.chunk.js"
+  },
+  {
+    "revision": "7738be2ed5e9eda5ba72",
+    "url": "/static/css/main.40054886.chunk.css"
+  },
+  {
+    "revision": "1c177d550cae603a1d33de407acaed9b",
+    "url": "/index.html"
+  }
+];
---
+++ routes/api-routes.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const axios = require("axios");
+const db = require("../models");
+const path = require("path");
+
+module.exports = function(app) {
+    app.get("/api/books", (req, res) => {
+        db.Book.find().then(
+            (booksData) => {
+                res.json(booksData);
+            }
+        ).catch(
+            (err) => {
+                res.json({error: err});
+            }
+        );
+    });
+
+    app.post("/search", (req, res) => {
+        // set bookTitle to the req.body.title with spaces replaced with plus signs(+)
+        let bookTitle = req.body.title.replace(/\s/g, "+");
+        let GBOOKS_KEY='AIzaSyDWMhMX2b41poApzFWMIcanjV5WIiciwR8';
+        axios.get(
+            // <script src="https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse"></script>
+
+            `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${GBOOKS_KEY}`
+        ).then(
+            (response) => {
+                res.json(response.data.items)
+            }
+        ).catch(
+            (err) => {
+                res.json({error: error})
+            }
+        );
+    });
+
+    app.post("/api/books", (req, res) => {
+        db.Book.create(req.body).then(
+            (response) => {
+                res.json({successful: response});
+            }
+        ).catch(
+            (err) => {
+                res.json({error: err});
+            }
+        );
+    });
+
+    app.delete("/api/books/:id", (req, res) => {
+        db.Book.findByIdAndDelete(req.params.id).then(
+            (response) => {
+                res.json({successful: response});
+            }
+        ).catch(
+            (err) => {
+                rres.json({error: err});
+            }
+        );
+    });
+
+    // Send every other request to the React app
+    // Define any API routes before this runs
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+}
