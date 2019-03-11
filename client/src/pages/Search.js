355d503b178f363eb34bc00d2ffae942da87eadb
Huiying0108 <whymissu1228@gmail.com>
2019-03-11 14:49:55 -0700

    search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchForm from "../components/SearchForm";
 import ResultsContainer from "../components/ResultsContainer";
-import API from "../utils/API";
+import API from "./../utils/API";
 
 class Search extends React.Component {
     constructor(props) {
