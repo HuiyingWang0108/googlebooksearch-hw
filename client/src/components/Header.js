43db2d811e4bc2847fd2b4bd689aac57a94c9c88
Huiying Wang <36682988+HuiyingWang0108@users.noreply.github.com>
2019-03-20 12:42:23 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,7 +13,6 @@ function Header() {
     return (
         <header style={headerStyle}>
             <h1>(React) Google Books Search</h1>
-            <p>Search for and Save Books of Interest</p>
         </header>
     );
 }
