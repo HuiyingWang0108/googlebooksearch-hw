6a0e2a48e4cda8ce46a5712447df8046137a3cea
Huiying Wang <36682988+HuiyingWang0108@users.noreply.github.com>
2019-03-21 08:44:41 -0700

    Update and rename Nav.js to Navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+      return (
+            <nav class="navbar navbar-light bg-light">
+                  <span class="navbar-brand mb-0 h1">Google Books</span>
+                  <nav class="navbar navbar-light bg-light">
+                        <a class="navbar-brand" href="/">Search</a>
+                        <a class="navbar-brand" href="/saved">Saved</a>
+                        {/* <Link to="/">Search</Link>
+                        <Link to="/saved">Saved</Link> */}
+                  </nav>
+            </nav>
+      );
+}
+
+export default Navbar;
